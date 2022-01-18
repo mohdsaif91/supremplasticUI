6c7ba76f8cf40993356fefa3ccc21a349593feb4
Mutallib <qmutallib2002@gmail.com>
2022-01-18 22:44:03 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Registration from "./components/Registration";
 import Login from "./components/Login";
-import { Container } from "react-bootstrap";
-import About from "./components/About";
-import ContactUs from "./components/ContactUs";
+// import { Container } from "react-bootstrap";
+// import About from "./components/About";
+// import ContactUs from "./components/ContactUs";
 // import ViewNotes from "./components/ViewNotes";
-import Profile from "./components/Profile";
+// import Profile from "./components/Profile";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -22,17 +22,17 @@ function App() {
         <Registration />
       <Profile/>
       </Container> */}
-      <Container>
+      {/* <Container> */}
         <Routes>
-          <Route path="/About" element={<About />} exact />
-          <Route path="/ContactUs" element={<ContactUs/>} exact/>
+          {/* <Route path="/About" element={<About />} exact /> */}
+          {/* <Route path="/ContactUs" element={<ContactUs/>} exact/> */}
           {/* <Route path="/ViewNotes" element={<ViewNotes/>} exact/> */}
-          <Route path="/Profile" element={<Profile/>} exact/>
+          {/* <Route path="/Profile" element={<Profile/>} exact/> */}
           <Route path="/Registration" element={<Registration/>} exact/>
           <Route path="/Login" element={<Login/>} exact/>
           <Route path="/" element={<Home />} exact />
         </Routes>
-        </Container>
+        {/* </Container> */}
       </BrowserRouter>
   );
 }
